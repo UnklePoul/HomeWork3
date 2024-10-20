9289ed4ca67fe06c919eaa856cfb315fdd6bc431
«Poul» <overlord.est@mail.ru>
2024-10-21 03:29:58 +1200

    exercise 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-    int myInt = 10 ;
-    byte myByte = 5 ;
-    short myShort = 20 ;
-    long myLong = 10000000L ;
-    float myFloat = 10.2f ;
-    double myDouble = 10.7  ;
-
-        System.out.println(myInt);
-        System.out.println(myByte);
-        System.out.println(myShort);
-        System.out.println(myLong);
-        System.out.println(myFloat);
-        System.out.println(myDouble);
-    }
-}
+    double a  = 27.12 ;
+    long b = 987_678_965_549L ;
+    float c = 2.786f ;
+    int d = 569 ;
+    short e = -159 ;
+    int f  = 27897  ;
+    byte g = 67
