57dfe29e6747b548a08704983b88f7f95a5ab198
«Poul» <overlord.est@mail.ru>
2024-10-21 04:49:40 +1200

    exercise 8

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int wiegtLossGoalKg = 7;
-        double dailyWiegtLossLowKg = 0.25;
-        double dailyWiegtLossHighKg = 0.5;
+        int mashaSalary = 67760;
+        int denisSalary = 83690;
+        int kristinaSalary = 76230;
 
-        int daysLow = (int) Math.ceil(wiegtLossGoalKg / dailyWiegtLossLowKg);
-        int daysHigh = (int) Math.ceil(wiegtLossGoalKg / dailyWiegtLossHighKg);
-        int daysAverge = (daysLow + daysHigh) / 2;
+        int mashaNewSalary = (int) (mashaSalary * 1.1);
+        int denisNewSalary = (int) (denisSalary * 1.1);
+        int kristinaNewSalary = (int) (kristinaSalary * 1.1);
 
-        System.out.println("Теряем в день: " + dailyWiegtLossLowKg + " нужно дней " + daysLow);
-        System.out.println("Теряем в день: " + dailyWiegtLossHighKg + " нужно дней " + daysHigh);
-        System.out.println("Среднее количестов дней: " + daysAverge);
+        int mashaSalaryBeforIndexatione = mashaSalary * 12;
+        int denisSalaryBeforIndexatione = denisSalary * 12;
+        int kristinaSalaryBeforIndexatione = kristinaSalary * 12;
+
+        int mashaSalaryAfterIndexatione = mashaNewSalary * 12;
+        int denisSalaryAfterIndexatione = denisNewSalary * 12;
+        int kristinaSalaryAfterIndexatione = kristinaNewSalary * 12;
+
+        System.out.println("Маша теперь получает "+mashaNewSalary+ "доход вырос на "+ (mashaSalaryAfterIndexatione-mashaSalaryBeforIndexatione));
+        System.out.println("Денис теперь получает "+denisNewSalary+ "доход вырос на "+ (denisSalaryAfterIndexatione-denisSalaryBeforIndexatione));
+        System.out.println("Кристина теперь получает "+kristinaNewSalary+ "доход вырос на "+ (kristinaSalaryAfterIndexatione-kristinaSalaryBeforIndexatione));
 
     }}
